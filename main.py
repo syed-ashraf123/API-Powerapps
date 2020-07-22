f5388c8c7c2b38d377ed9b333f6991abb2ca421b
SYED OSAMA ASHRAF <47147934+SyedOsamaAshraf@users.noreply.github.com>
2020-07-22 20:23:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
-@app.route('/<variable>')
-def profile(variable):
-	return variable
+
 @app.route('/')
 def profile1():
 	return "OK Request coming"
